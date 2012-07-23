046518a6a5cf9bd25bd381e693c72f9bb59d9e2f
Colin Newell <colin@opusvl.com>
2012-07-23 10:55:00 +0100

    Bumped version number

--- lib/OpusVL/AppKitX/SysParams.pm
+++ lib/OpusVL/AppKitX/SysParams.pm
@@ -4,7 +4,7 @@ use CatalystX::InjectComponent;
 use File::ShareDir qw/module_dir/;
 use namespace::autoclean;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 after 'setup_components' => sub {
     my $class = shift;
